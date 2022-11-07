76e8089407bf82f2ffe0ad1a5383a08fc6cb0c82
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2022-11-08 09:24:52 +1100

    add build ns3 script

--- controller/sim_script/build_ns3.py
+++ controller/sim_script/build_ns3.py
@@ -4,5 +4,5 @@ from os.path import expanduser
 from sim_src.ns3_ctrl.ns3_ctrl import build_ns3
 
 home = expanduser("~")
-ns3_path = os.path.join(home,"ns-3-dev")
+ns3_path = os.path.join(home,"wifi-ai/ns-3-dev")
 build_ns3(ns3_path)

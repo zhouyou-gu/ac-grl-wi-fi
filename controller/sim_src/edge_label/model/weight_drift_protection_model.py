adf19f8254961f1c074bce7d12a1a65ba7607477
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2022-11-08 04:40:30 +1100

    new wdp model

---
+++ controller/sim_src/edge_label/model/weight_drift_protection_model.py
@@ -0,0 +1,47 @@
+from base_model import *
+
+
+class weight_drift_protection(base_model):
+    def _train_actor(self,batch):
+        self._print("_train_actor")
+        loss = torch.zeros(1)
+        for sample in batch:
+            G = nx.complete_graph(sample['n_node'])
+            e_index:torch.Tensor = from_networkx(G).edge_index
+            with torch.no_grad():
+                state = to_tensor(sample['state'],requires_grad=False)
+                state = torch.hstack((state[e_index[0,:]],state[e_index[1,:]]))
+
+                actor_input = state
+
+                x = to_tensor(sample['state'],requires_grad=False)
+
+                label = self.actor.forward(actor_input)
+
+            label_no_grad = label.clone()
+            label.requires_grad_()
+
+            s_a = torch.hstack((state,label))
+            self._printa("_train_actor states\n",to_numpy(state).T)
+            self._printa("_train_actor sapair\n",to_numpy(s_a).T)
+
+            q = self.critic_target.forward(x,e_index,label)
+            q = -torch.mean(q)
+            q.backward()
+            lable_g = label.grad.detach()
+            scale = label_no_grad
+            scale[lable_g>0.] = 1. - scale[lable_g>0.]
+            label_differentiable = self.actor.forward(actor_input)
+            self._printa("_train_actor label.grad\n",to_numpy(label.grad).T)
+            self._printa("_train_actor lable_g\n",to_numpy(lable_g).T)
+            self._printa("_train_actor scale\n",to_numpy(scale).T)
+
+            loss += (-torch.mean((lable_g*scale)*label_differentiable))
+
+        loss/=len(batch)
+        self._print("_train_actor loss",loss)
+        self.actor_optim.zero_grad()
+        loss.backward()
+        self.actor_optim.step()
+
+        return to_numpy(loss)

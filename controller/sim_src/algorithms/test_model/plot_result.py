10025d96c8f82cd7bcb4f3aca3d969ecb41677a7
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2022-11-13 02:25:35 +1100

    plot data

--- controller/sim_src/algorithms/test_model/plot_result.py
+++ controller/sim_src/algorithms/test_model/plot_result.py
@@ -24,12 +24,33 @@ def load_res_dir_parm(dir,cls_name,postfix):
     d_file = os.path.join(dir,d_name)
     return np.genfromtxt(d_file,delimiter=',',dtype=[("name", str, 15),("value",float)]).tolist()
 
-def get_uf_for_alpha(data,a,m_avg = 20):
-    res = np.min(data,axis=1)
+def get_uf_value(data,a,m_avg = 50):
     if a == 1:
         data = np.log(data+0.001)
+        data = np.mean(data,axis=1)
+        res = np.exp(data)
     else:
-        data = np.power(data+0.001,1-a)/(1-a)
+        data = np.power(data+0.001,1-a)
+        data = np.mean(data,axis=1)
+        res = np.power(data,1./(1.-a))
+    res = np.convolve(res, np.ones(m_avg)/m_avg, mode='same')
+    return res
+
+def get_min_rate(data,a,m_avg = 50):
+    res = np.min(data,axis=1)
+    res = np.convolve(res, np.ones(m_avg)/m_avg, mode='same')
+    return res
+
+
+def get_op(data,a,m_avg = 50):
+    data[data>0.05] = 1.
+    data = 1-data
+    res = np.mean(data,axis=1)
+    res = np.convolve(res, np.ones(m_avg)/m_avg, mode='same')
+    return res
+
+def get_mean_rate(data,a,m_avg = 50):
+    res = np.mean(data,axis=1)
     res = np.convolve(res, np.ones(m_avg)/m_avg, mode='same')
     return res
 
@@ -45,10 +66,10 @@ for t in TEST_LIST:
         ax = plt.gca()
         print(ax.get_position())
         ax.set_position([0.05, 0.05, 0.55, 0.9])
-        FIG_NAME = "%s.ALPHA.%s.jpg"%(DIR,a)
+        FIG_NAME = "%s.ALPHA.%s.uf_value.jpg"%(DIR,a)
         fig.suptitle(FIG_NAME)
         ax.set_xlim([0, N_STEP])
-        ax.set_ylim([0, 0.2])
+        ax.set_ylim([0, 1])
         for dir in ALL_DIR:
             data = load_res_dir_data(os.path.join(DIR_PATH,dir),"sim_env","reward",N_STEP-1)
             print(dir)
@@ -56,7 +77,92 @@ for t in TEST_LIST:
             alpha = int(alpha[1])
             print(alpha)
             if alpha == a:
-                res = get_uf_for_alpha(data[:,3:],a)
+                res = get_uf_value(data[:,3:],a)
+                plt.plot(data[:,1], res, label=dir)
+        plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
+        fig.savefig(os.path.join(DIR_PATH,FIG_NAME))
+
+ALL_ALPHA = [0, 1, 2, 5, 10]
+N_STEP = 1000
+for t in TEST_LIST:
+    DIR = os.path.splitext(t)[0]
+    DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), DIR)
+    print(DIR_PATH)
+    ALL_DIR = [dI for dI in os.listdir(DIR_PATH) if os.path.isdir(os.path.join(DIR_PATH,dI))]
+    for a in ALL_ALPHA:
+        fig = plt.figure(figsize=(16, 6), dpi=80)
+        ax = plt.gca()
+        print(ax.get_position())
+        ax.set_position([0.05, 0.05, 0.55, 0.9])
+        FIG_NAME = "%s.ALPHA.%s.min_rate.jpg"%(DIR,a)
+        fig.suptitle(FIG_NAME)
+        ax.set_xlim([0, N_STEP])
+        ax.set_ylim([0, 1])
+        for dir in ALL_DIR:
+            data = load_res_dir_data(os.path.join(DIR_PATH,dir),"sim_env","reward",N_STEP-1)
+            print(dir)
+            alpha = load_res_dir_parm(os.path.join(DIR_PATH,dir),"sim_config","NaN")
+            alpha = int(alpha[1])
+            print(alpha)
+            if alpha == a:
+                res = get_min_rate(data[:,3:],a)
+                plt.plot(data[:,1], res, label=dir)
+        plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
+        fig.savefig(os.path.join(DIR_PATH,FIG_NAME))
+
+
+ALL_ALPHA = [0, 1, 2, 5, 10]
+N_STEP = 1000
+for t in TEST_LIST:
+    DIR = os.path.splitext(t)[0]
+    DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), DIR)
+    print(DIR_PATH)
+    ALL_DIR = [dI for dI in os.listdir(DIR_PATH) if os.path.isdir(os.path.join(DIR_PATH,dI))]
+    for a in ALL_ALPHA:
+        fig = plt.figure(figsize=(16, 6), dpi=80)
+        ax = plt.gca()
+        print(ax.get_position())
+        ax.set_position([0.05, 0.05, 0.55, 0.9])
+        FIG_NAME = "%s.ALPHA.%s.outage_prob.jpg"%(DIR,a)
+        fig.suptitle(FIG_NAME)
+        ax.set_xlim([0, N_STEP])
+        ax.set_ylim([0, 0.02])
+        for dir in ALL_DIR:
+            data = load_res_dir_data(os.path.join(DIR_PATH,dir),"sim_env","reward",N_STEP-1)
+            print(dir)
+            alpha = load_res_dir_parm(os.path.join(DIR_PATH,dir),"sim_config","NaN")
+            alpha = int(alpha[1])
+            print(alpha)
+            if alpha == a:
+                res = get_op(data[:,3:],a)
+                plt.plot(data[:,1], res, label=dir)
+        plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
+        fig.savefig(os.path.join(DIR_PATH,FIG_NAME))
+
+ALL_ALPHA = [0, 1, 2, 5, 10]
+N_STEP = 1000
+for t in TEST_LIST:
+    DIR = os.path.splitext(t)[0]
+    DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), DIR)
+    print(DIR_PATH)
+    ALL_DIR = [dI for dI in os.listdir(DIR_PATH) if os.path.isdir(os.path.join(DIR_PATH,dI))]
+    for a in ALL_ALPHA:
+        fig = plt.figure(figsize=(16, 6), dpi=80)
+        ax = plt.gca()
+        print(ax.get_position())
+        ax.set_position([0.05, 0.05, 0.55, 0.9])
+        FIG_NAME = "%s.ALPHA.%s.sum_rate.jpg"%(DIR,a)
+        fig.suptitle(FIG_NAME)
+        ax.set_xlim([0, N_STEP])
+        ax.set_ylim([0, 0.5])
+        for dir in ALL_DIR:
+            data = load_res_dir_data(os.path.join(DIR_PATH,dir),"sim_env","reward",N_STEP-1)
+            print(dir)
+            alpha = load_res_dir_parm(os.path.join(DIR_PATH,dir),"sim_config","NaN")
+            alpha = int(alpha[1])
+            print(alpha)
+            if alpha == a:
+                res = get_mean_rate(data[:,3:],a)
                 plt.plot(data[:,1], res, label=dir)
         plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
         fig.savefig(os.path.join(DIR_PATH,FIG_NAME))

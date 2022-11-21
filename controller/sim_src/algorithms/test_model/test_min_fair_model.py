bf62ae55a04e20e8f7523638d4594d5e913def57
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2022-11-21 19:47:13 +1100

    min fairness

--- controller/sim_src/algorithms/test_model/test_min_fair_model.py
+++ controller/sim_src/algorithms/test_model/test_min_fair_model.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 from sim_src.algorithms.test_model.run_test import run_test
-from sim_src.edge_label.model.base_model import base_model
+from sim_src.edge_label.model.complete_model import complete_model
 from sim_src.util import GET_LOG_PATH_FOR_SIM_SCRIPT
 
 np.set_printoptions(threshold=5)
@@ -13,7 +13,7 @@ np.set_printoptions(linewidth=1000)
 torch.set_printoptions(threshold=5)
 torch.set_printoptions(linewidth=1000)
 
-model_class = base_model
+model_class = complete_model
 run_test(model_class,GET_LOG_PATH_FOR_SIM_SCRIPT(__file__),ALPHA=100)
 time.sleep(5)
 

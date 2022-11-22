337eb4a698f1731e1996a4817cf98adb16492470
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2022-11-23 04:54:18 +1100

    minor debug

--- controller/sim_src/edge_label/model/base_model.py
+++ controller/sim_src/edge_label/model/base_model.py
@@ -25,7 +25,7 @@ class base_model(learning_model):
     CRI_LR = 0.001
     TAU = 0.01
     FAIRNESS_ALPHA = 10
-    MAX_FAIR_ALPHA = 10
+    MAX_FAIR_ALPHA = 50
     EXPLORATION = False
     EXPLORATION_PROB = 0.1
     def __init__(self, id, edge_dim=1, node_dim=4):
@@ -127,13 +127,17 @@ class base_model(learning_model):
         self._printa("_fair_q qraw",q.transpose(0,1))
         if self.FAIRNESS_ALPHA == 1:
             q = torch.log(q)
-            q = torch.mean(q,dim=1,keepdim=True)
+            q = torch.mean(q,dim=0,keepdim=True)
+            # q = torch.exp(q)
+            self._printa("_fair_q fair pf",q.transpose(0,1))
         elif self.FAIRNESS_ALPHA > self.MAX_FAIR_ALPHA:
-            q, _ = torch.min(q,dim=1,keepdim=True)
+            q, _ = torch.min(q,dim=0,keepdim=True)
+            self._printa("_fair_q fair min",q.transpose(0,1))
         else:
             q = torch.pow(q,1-self.FAIRNESS_ALPHA)/(1-self.FAIRNESS_ALPHA)
-            q = torch.mean(q,dim=1,keepdim=True)
-        self._printa("_fair_q fair",q.transpose(0,1))
+            q = torch.mean(q,dim=0,keepdim=True)
+            # q = torch.pow(q*(1-self.FAIRNESS_ALPHA),1/(1-self.FAIRNESS_ALPHA))
+            self._printa("_fair_q fair",self.FAIRNESS_ALPHA,q.transpose(0,1))
         return q
     def setup_infer(self):
         # NOT USED IN BASE MODEL

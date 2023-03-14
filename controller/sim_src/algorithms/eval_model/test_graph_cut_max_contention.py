0fc38df54461888fdfcb96b72b4776e2cc4efb23
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2023-03-14 19:35:16 +1100

    add max contention

---
+++ controller/sim_src/algorithms/eval_model/test_graph_cut_max_contention.py
@@ -0,0 +1,46 @@
+import os
+import random
+from os.path import expanduser
+
+import numpy as np
+import torch
+
+from sim_src.other_methods.graphcut_model import graphcut_contention_model
+from sim_src.sim_env.sim_env import sim_env
+from sim_src.util import GET_LOG_PATH_FOR_SIM_SCRIPT, ParameterConfig, StatusObject
+
+np.set_printoptions(threshold=5)
+np.set_printoptions(linewidth=1000)
+
+torch.set_printoptions(threshold=5)
+torch.set_printoptions(linewidth=1000)
+
+StatusObject.DISABLE_ALL_DEBUG = False
+
+
+OUT_FOLDER = GET_LOG_PATH_FOR_SIM_SCRIPT(__file__)
+
+e = sim_env(id=random.randint(40,200))
+
+ns3_path = os.path.join(expanduser("~"),"wifi-ai/ns-3-dev")
+e.PROG_PATH = ns3_path
+e.PROG_NAME = "wifi-ai/env"
+e.DEBUG = True
+
+
+n_step = 1000
+model = graphcut_contention_model(0)
+model.DEBUG_STEP = 10
+model.DEBUG = True
+
+cfg = ParameterConfig()
+# cfg['ALPHA'] = ALPHA
+# model.FAIRNESS_ALPHA = cfg['ALPHA']
+cfg.save(OUT_FOLDER,"NaN")
+
+e.set_actor(model)
+for i in range(n_step):
+    e.init_env()
+    sample = e.step(run_ns3=True)
+    if (i+1) in [10, 100, 500, 1000]:
+        e.save_np(OUT_FOLDER,str(i))
--- controller/sim_src/other_methods/graphcut_model.py
+++ controller/sim_src/other_methods/graphcut_model.py
@@ -92,6 +92,40 @@ class graphcut_hidden_model(infer_model):
         return action
 
 
+class graphcut_contention_model(infer_model):
+    MIN_RSSI = -95
+    def gen_action(self,state_np):
+        n_node = state_np.shape[0]
+        with torch.no_grad():
+            G = nx.complete_graph(n_node)
+            e_index = to_device(from_networkx(G).edge_index)
+            state = to_tensor(state_np)
+            state = torch.hstack((state[e_index[0,:]],state[e_index[1,:]]))
+
+            result = self.infer_target.forward(state)
+            p_contention = result
+
+            state = to_tensor(state_np,requires_grad=False)
+            min_path_loss, min_path_loss_idx = torch.min(state,dim=1,keepdim=True)
+
+            # state_A = state[e_index[0,:]]
+            # state_A_to_B = torch.gather(state_A,1,min_path_loss_idx[e_index[1,:]])
+            state_B = state[e_index[1,:]]
+            state_B_to_A = torch.gather(state_B,1,min_path_loss_idx[e_index[0,:]])
+
+
+            interference = (state_B_to_A >= 0.1)
+
+            # print(interference)
+            # label = interference.float()
+            # print(label)
+            label = (p_contention)
+
+            action_mat = to_dense_adj(edge_index=e_index,batch=None,edge_attr=label).view(n_node,n_node)
+            action = to_numpy(action_mat)
+
+        return action
+
 if __name__ == '__main__':
     np.set_printoptions(threshold=np.inf)
     np.set_printoptions(linewidth=np.inf)
